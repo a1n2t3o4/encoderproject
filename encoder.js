736a4ad27ce67ba2f4c359b9c8df4bee265879d1
Anto A <antraniga@gmail.com>
2016-10-11 13:51:56 -0700

    1

---
+++ encoder.js
@@ -0,0 +1,8 @@
+$("#inputText").keydown(function(e) {
+  e.keyCode == 13;
+});
+
+$("input[type='radio']").click(function() {
+  $("input:checked").prop('checked', false);
+  $(this).prop('checked', true);
+})
